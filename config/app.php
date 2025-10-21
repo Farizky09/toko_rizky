3a6be7e9e3f1cc82dbf6f270d2f48eb234ddff75
Ahmad Farizky <94207555+Farizky09@users.noreply.github.com>
2025-10-22 01:52:23 +0700

    set up installation

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Jakarta',
 
     /*
     |--------------------------------------------------------------------------
@@ -121,6 +121,12 @@ return [
     'maintenance' => [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
+    ],
+
+
+    'providers' => [
+        // ...
+        Yajra\DataTables\DataTablesServiceProvider::class,
     ],
 
 ];
